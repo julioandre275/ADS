82f33bcc21bad6ea272c10119a4423f38bf3a11b
julioandre275 <jpokl1@student.monash.edu>
2017-12-09 21:47:59 +0800

    insertion sort

---
+++ S0rts.py
@@ -0,0 +1,32 @@
+class Sorts:
+    """
+    CLass of sorting algorithms
+    Author: Okley Julio Andreotti Papafio
+    """
+    def __init__(self,array):
+        self.array = array
+    #Swap function to swap elements in an array
+    def swap(self, j, i):
+        tmp = self.array[j]
+        self.array[j] = self.array[i]
+        self.array[i] = tmp
+    ## Insertion sort function
+    ## Complexity O(N)2
+    def insertion_sort(self):
+        i = 1
+        for i in range(len(self.array)):
+            j = i-1
+            while j>=0:
+                if self.array[i]<self.array[j]:
+                    self.swap(i,j)
+                    i = i-1
+                j= j-1
+
+    def outting(self):
+        print(self.array)
+
+
+
+newthing = Sorts([3,4,6,78,43,54,12,34,56,78,90,12,1300,233,424,789,5,567,12,34,2,1,0,7,65,3342,567,2346,78,54,3,45,75,32,12,21,15,76,42])
+newthing.insertion_sort()
+newthing.outting()
